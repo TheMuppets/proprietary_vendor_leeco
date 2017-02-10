839343c8c85efc78eac3811f10ef03f3d1e6dd9b
Sahil Soni <sscsps@gmail.com>
2017-02-10 18:25:52 +0530

    added x500 blobs

---
+++ x500/x500-vendor-blobs.mk
@@ -0,0 +1,15 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LeTV/x500/proprietary/app,system/app)
+
+
