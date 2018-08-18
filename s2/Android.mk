4dfb25d4cb317e443fc11d5dc616001a395f5090
Rk779 <Rahulkrishna585@gmail.com>
2018-08-18 11:29:30 +0000

    s2: Update display stack to O Extracted from SRT Phone Fingerprint:Smartron/srtphone/rimo02a:8.0.0/T5524INDURC-475/475:user/release-keys
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>

--- s2/Android.mk
+++ s2/Android.mk
@@ -30,6 +30,17 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := vendor.display.color@1.0
+LOCAL_MODULE_OWNER := leeco
+LOCAL_SRC_FILES_64 := proprietary/lib64/vendor.display.color@1.0.so
+LOCAL_SRC_FILES_32 := proprietary/lib/vendor.display.color@1.0.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := leeco
 LOCAL_SRC_FILES := proprietary/lib64/libloc_api_v02.so
