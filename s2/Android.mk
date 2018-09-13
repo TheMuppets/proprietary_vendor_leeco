0e8c8171b8c60d55cde86a5f73ef95afeb7aef02
Rk779 <Rahulkrishna585@gmail.com>
2018-09-13 15:06:41 +0000

    s2: Update QTI bluetooth stack
    Change-Id: Idca9c39799946db73e176736b3d747c2e4d5cd04
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>

--- s2/Android.mk
+++ s2/Android.mk
@@ -30,6 +30,17 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libantradio
+LOCAL_MODULE_OWNER := leeco
+LOCAL_SRC_FILES_64 := proprietary/lib64/libantradio.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libantradio.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.display.color@1.0
 LOCAL_MODULE_OWNER := leeco
 LOCAL_SRC_FILES_64 := proprietary/lib64/vendor.display.color@1.0.so
