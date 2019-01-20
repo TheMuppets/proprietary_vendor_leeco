e40d0d847531fe706a91cba27a9b9f87ed2a4b51
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2019-01-20 02:23:20 +0000

    msm8996-common: implement binderized consumerir HAL + bridge
    Since LeEco doesn't use any driver blobs for consumerir and instead
    uses a quickset SDK, we need a service that acts as a bridge to the
    quickset SDK for translating calls from our consumerir  HAL,  which
    we named "ConsumerirTransmitter".
    
    How this works is our consumerir HAL receives an IR command,  which
    it  sends  to the ConsumerirTransmitter that translates it  to  the
    quickset SDK.
    
    Change-Id: Ib21a70708cdfb2c7f6a53c8a1c6ddcac830a521e

--- msm8996-common/msm8996-common-vendor.mk
+++ msm8996-common/msm8996-common-vendor.mk
@@ -148,6 +148,7 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/msm8996-common/proprietary/vendor/bin/time_daemon:$(TARGET_COPY_OUT_VENDOR)/bin/time_daemon \
     vendor/leeco/msm8996-common/proprietary/vendor/bin/wcnss_filter:$(TARGET_COPY_OUT_VENDOR)/bin/wcnss_filter \
     vendor/leeco/msm8996-common/proprietary/vendor/bin/xtra-daemon:$(TARGET_COPY_OUT_VENDOR)/bin/xtra-daemon \
+    vendor/leeco/msm8996-common/proprietary/vendor/etc/UEISettings:$(TARGET_COPY_OUT_VENDOR)/etc/UEISettings \
     vendor/leeco/msm8996-common/proprietary/vendor/etc/cacert_location.pem:$(TARGET_COPY_OUT_VENDOR)/etc/cacert_location.pem \
     vendor/leeco/msm8996-common/proprietary/vendor/etc/cne/wqeclient/ATT/ATT_profile1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/cne/wqeclient/ATT/ATT_profile1.xml \
     vendor/leeco/msm8996-common/proprietary/vendor/etc/cne/wqeclient/ATT/ATT_profile2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/cne/wqeclient/ATT/ATT_profile2.xml \
@@ -675,4 +676,5 @@ PRODUCT_PACKAGES += \
     dpmserviceapp \
     ims \
     qcrilmsgtunnel \
-    TimeService
+    TimeService \
+    UEIQuicksetSDKLeTV
