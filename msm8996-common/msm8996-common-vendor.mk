698a7c7dedf04123d0f59e5a462e28ba70d75bab
Marc Bourgoin <themard85@gmail.com>
2019-02-20 19:59:28 -0700

    Revert "msm8996-common: implement binderized consumerir HAL + bridge"
    This reverts commit e40d0d847531fe706a91cba27a9b9f87ed2a4b51.
    
    * using a vendor init script to copy from vendor to /data/data/ (which is a core_data_file) is a neverallow
    * these files will need to be moved to system to workaround this issue, but this commit is causing issues with extracting files
    * once consumerir HAL + bridge is reworked to adhere to selinux neverallow policies, we can add the required files back as that commit also includes the proprietary file changes.

--- msm8996-common/msm8996-common-vendor.mk
+++ msm8996-common/msm8996-common-vendor.mk
@@ -154,7 +154,6 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/msm8996-common/proprietary/vendor/bin/thermal-engine:$(TARGET_COPY_OUT_VENDOR)/bin/thermal-engine \
     vendor/leeco/msm8996-common/proprietary/vendor/bin/time_daemon:$(TARGET_COPY_OUT_VENDOR)/bin/time_daemon \
     vendor/leeco/msm8996-common/proprietary/vendor/bin/xtra-daemon:$(TARGET_COPY_OUT_VENDOR)/bin/xtra-daemon \
-    vendor/leeco/msm8996-common/proprietary/vendor/etc/UEISettings:$(TARGET_COPY_OUT_VENDOR)/etc/UEISettings \
     vendor/leeco/msm8996-common/proprietary/vendor/etc/cacert_location.pem:$(TARGET_COPY_OUT_VENDOR)/etc/cacert_location.pem \
     vendor/leeco/msm8996-common/proprietary/vendor/etc/cne/wqeclient/ATT/ATT_profile1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/cne/wqeclient/ATT/ATT_profile1.xml \
     vendor/leeco/msm8996-common/proprietary/vendor/etc/cne/wqeclient/ATT/ATT_profile2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/cne/wqeclient/ATT/ATT_profile2.xml \
@@ -680,5 +679,4 @@ PRODUCT_PACKAGES += \
     dpmserviceapp \
     ims \
     qcrilmsgtunnel \
-    TimeService \
-    UEIQuicksetSDKLeTV
+    TimeService
