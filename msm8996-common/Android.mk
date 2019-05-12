f231a856250f2705936b06dc1b6ce048f86c83e0
Davide Garberi <dade.garberi@gmail.com>
2019-05-12 12:40:15 +0200

    msm8996-common: Update blobs to LA.UM.7.5.r1-04800-8x96.0
    Change-Id: I21c1c4a0f6e071cd82803f9fff8f0cce7cd16d94

--- msm8996-common/Android.mk
+++ msm8996-common/Android.mk
@@ -31,18 +31,6 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libgpustats
-LOCAL_MODULE_OWNER := leeco
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libgpustats.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libgpustats.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libsdm-disp-vndapis
 LOCAL_MODULE_OWNER := leeco
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libsdm-disp-vndapis.so
